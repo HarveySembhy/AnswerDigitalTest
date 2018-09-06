ce8ed0e3f50057a987c1ded37d1da86969671249
HarveySembhy <h.sembhy@gmail.com>
2018-09-06 16:04:16 +0100

    Added and polish scenarois for the login page

--- AnswerDigitalTest/FormAuthentication.cs
+++ AnswerDigitalTest/FormAuthentication.cs
@@ -22,11 +22,20 @@ namespace AnswerDigitalTest
          Scenario 3: Try to login with correct username and password and then logout
 
         */
+        //[SetUp]
+        //public void OpenBrowser()
+        //{
+        //    //Open the Chrome and go to Url
+        //    driver = new ChromeDriver(@"C:\driver");
+        //    driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/");
+        //    driver.Manage().Window.Maximize();
+        //}
+
 
         [Test]
         public void Scenario1()
         {
-            //Open the Chrome and go to Url
+            // Open the Chrome and go to Url
             driver = new ChromeDriver(@"C:\driver");
             driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/");
             driver.Manage().Window.Maximize();
@@ -40,61 +49,99 @@ namespace AnswerDigitalTest
 
             //Find Password and type in "harvey"
             IWebElement Password = driver.FindElement(By.Id("password"));
-            Password.SendKeys("harvey");
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5000);
+            Password.SendKeys("harvey");            
 
             //Find Login button and click
             IWebElement Login = driver.FindElement(By.XPath("//div[2]/div/div/form/button"));
             Login.Click();
 
-            
+            //Set a wait period
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
             //Verify username invalid error
-            //IWebElement Error = driver.FindElement(By.Id(""));
+            IWebElement Error = driver.FindElement(By.Id("flash"));
 
-            //Close browser - End Test
-            driver.Close();
+            //Set a wait period
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
 
-
-        }             
-
+            driver.Quit();
+        }
+        
         [Test]
         public void Scenario2()
         {
-            //Open the Chrome and go to Url
-            driver = new ChromeDriver(@"C:\driver");
-
-            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/");
-
-            //Find and click on Form Authentication
-            IWebElement FormAuth = driver.FindElement(By.XPath("//*[@id='content']/ul/li[18]/a"));
-
-            FormAuth.Click();
-
-
-
-
-
-            //Close browser - End Test
-            driver.Close();
-
-
-        }
-
-        [Test]
-        public void Scenario3()
-        {
-            //Open the Chrome and go to Url
+            // Open the Chrome and go to Url
             driver = new ChromeDriver(@"C:\driver");
             driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/");
+            driver.Manage().Window.Maximize();
 
             //Find and click on Form Authentication
             driver.FindElement(By.XPath("//*[@id='content']/ul/li[18]/a")).Click();
 
+            //Find Username and type in "tomsmith"
+            IWebElement Username = driver.FindElement(By.Id("username"));
+            Username.SendKeys("harveysembhy");
 
+            //Find Password and type in "harvey"
+            IWebElement Password = driver.FindElement(By.Id("password"));
+            Password.SendKeys("SuperSecretPassword!");
 
-            //Close browser - End Test
-            driver.Close();
+            //Find Login button and click
+            IWebElement Login = driver.FindElement(By.XPath("//div[2]/div/div/form/button"));
+            Login.Click();
+
+            //Set a wait period
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+            //Verify Success message
+            IWebElement Success = driver.FindElement(By.Id("flash"));
+                      
+
+            //Veryfi you are on the Login Page
+            IWebElement LoginPageText = driver.FindElement(By.Id("flash"));
+
+            //Set a wait period
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+            driver.Quit();
         }
+
+        
+        [Test]
+        public void Scenario3()
+        {
+            // Open the Chrome and go to Url
+            driver = new ChromeDriver(@"C:\driver");
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/");
+            driver.Manage().Window.Maximize();
+
+            //Find and click on Form Authentication
+            driver.FindElement(By.XPath("//*[@id='content']/ul/li[18]/a")).Click();
+
+            //Find Username and type in "tomsmith"
+            IWebElement Username = driver.FindElement(By.Id("username"));
+            Username.SendKeys("tomsmith");
+
+            //Find Password and type in "harvey"
+            IWebElement Password = driver.FindElement(By.Id("password"));
+            Password.SendKeys("SuperSecretPassword!");
+
+            //Find Login button and click
+            IWebElement Login = driver.FindElement(By.XPath("//div[2]/div/div/form/button"));
+            Login.Click();
+
+            //Set a wait period
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+            //Logout button
+            IWebElement Logout = driver.FindElement(By.XPath("//div[2]/div/div/a"));
+            Logout.Click();
+
+            //Set a wait period
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+            driver.Quit();
+        }
+       
     }
 }
