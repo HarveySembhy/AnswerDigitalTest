50c231c69b82feb57dc2e7d97da8c9651b57eb44
HarveySembhy <h.sembhy@gmail.com>
2018-09-07 01:45:25 +0100

    Wrote a try and catch for the tests - hopefully these will execute the tests.

--- AnswerDigitalTest/Program.cs
+++ AnswerDigitalTest/Program.cs
@@ -13,12 +13,52 @@ namespace AnswerDigitalTest
     {
         static void Main(string[] args)
         {
+            var checkLogin = new FormAuthentication();
+            var checkScroll = new InfiniteScroll();
+            var checkKeyPress = new KeyPresses();
 
+            //Test
 
+            try
+            {
+                Console.WriteLine("Checking Login Tests...");
+                checkLogin.Scenario1();
+                checkLogin.Scenario2();
+                checkLogin.Scenario3();
+                Console.WriteLine("Login Tests have PASSSED");
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Login checks Failed");
+            }
 
+            try
+            {
+                Console.WriteLine("Checking Infinite Scrolling Tests...");
+                checkScroll.ScrollTwice();
+                Console.WriteLine("Scrolling tests have PASSED");
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Scrolling tests have Failed");
+            }
+
+            try
+            {
+                Console.WriteLine("Checking Key Press Tests...");
+                checkKeyPress.KeyPress();
+                Console.WriteLine("Key Press tests PASSED");
+            }
+
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Key Press test have Failed");
+            }
+
+            Console.ReadLine();
         }
         
-
+        
     }
     
 }
